e8c818a466ec9a455db922614dd9e7667a6a62c1
Abhishek singh <88192065+abhishekit04@users.noreply.github.com>
2021-08-27 19:47:11 +0530

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,22 @@
+int getSum(int a, int b) {
+    return (a + b);
+}
+
+int getDifference(int a, int b) {
+    return (a - b);
+}
+
+
+int getProduct(int a, int b) {
+    return (a * b);
+} 
+
+float getQuotent(float a, float b) {
+    return (a / b);
+}
+
+int getRemainder(int a, int b) {
+    return a % b;
+}
+
+
