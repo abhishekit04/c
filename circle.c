d27c404794309db32a183dfeee3681f02f33b8a2
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2021-10-26 20:05:44 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -2,11 +2,11 @@
 #include<conio.h>
 
 int main() {
-	    int gm = DETECT,gd;
-initgraph(&gm,&gd,''C:\\TC\\BGI'');
-circle(200,300,50);
-getch();
-closegraph();
-return 0;
+	int gm = DETECT,gd;
+	initgraph(&gm,&gd,''C:\\TC\\BGI'');
+	circle(200,300,50);
+	getch();
+	closegraph();
+	return 0;
 
 }
