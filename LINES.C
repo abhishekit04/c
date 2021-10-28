28999be5bdc301b5eb75137586d93ddbea7fd072
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2021-10-28 19:49:56 +0530

    Update LINES.C

--- LINES.C
+++ LINES.C
@@ -3,38 +3,38 @@
 #include<stdio.h>
 
 int main() {
-		int i;
-		int gm = DETECT,gd;
+	int i;
+	int gm = DETECT,gd;
 
 
 
-		initgraph(&gm,&gd,"C:\\TC\\BGI");
-		/*for(i=25;i<=125;i=i+20){
-			line(100,i,300,i);
-		}*/
-		outtextxy(400,20,"CHESS");
-		for(i=25;i<=225;i=i+25){
-			line(i,25,i,225);
-			line(25,i,225,i);
-		}
+	initgraph(&gm,&gd,"C:\\TC\\BGI");
+	/*for(i=25;i<=125;i=i+20){
+		line(100,i,300,i);
+	}*/
+	outtextxy(400,20,"CHESS");
+	for(i=25;i<=225;i=i+25){
+		line(i,25,i,225);
+		line(25,i,225,i);
+	}
 
 
-		line(400,400,300,400);
-		line(400,400,200,300);
-		line(200,300,300,400);
+	line(400,400,300,400);
+	line(400,400,200,300);
+	line(200,300,300,400);
 
-		line(300,100,300,300);
-		line(300,100,400,300);
-		line(400,300,300,300);
-
-
-		line(500,100,500,200);
-		line(500,100,400,200);
-		line(400,200,500,200);
+	line(300,100,300,300);
+	line(300,100,400,300);
+	line(400,300,300,300);
 
 
-		getch();
-		closegraph();
-		return 0;
+	line(500,100,500,200);
+	line(500,100,400,200);
+	line(400,200,500,200);
+
+
+	getch();
+	closegraph();
+	return 0;
 
 }
