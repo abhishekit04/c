f466edc79f299021d85b97f4d3113f111ebf6f57
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2021-10-28 19:49:32 +0530

    Update rect.c

--- rect.c
+++ rect.c
@@ -3,24 +3,22 @@
 #include<stdio.h>
 
 int main() {
-		int i;
-		int gm = DETECT,gd;
+	int i;
+	int gm = DETECT,gd;
+
+	initgraph(&gm,&gd,"C:\\TC\\BGI");
+
+
+	line(200,200,200,400);
+	line(200,400,400,400);
+	line(400,400,400,200);
+	line(200,200,400,200);
 
 
 
-		initgraph(&gm,&gd,"C:\\TC\\BGI");
-		
-
-		line(200,200,200,400);
-		line(200,400,400,400);
-		line(400,400,400,200);
-		line(200,200,400,200);
-
-
-
-		rectangle(400,50,500,100);
-		getch();
-		closegraph();
-		return 0;
+	rectangle(400,50,500,100);
+	getch();
+	closegraph();
+	return 0;
 
 }
