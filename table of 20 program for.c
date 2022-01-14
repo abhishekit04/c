0d63b0ccce803ae43639181b4d64b487207354dc
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2022-01-14 11:56:47 +0530

    Add files via upload

---
+++ table of 20 program for.c
@@ -0,0 +1,14 @@
+//program on table (for loop)
+
+#include<stdio.h>
+
+int main () {
+	int n=1;
+		int i=20;
+		printf("enter a number\n");
+		for(i=20;i<=10;i++) {
+			printf("%d\n",i);
+		}
+		
+		return 0;
+}
