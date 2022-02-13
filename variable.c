f58c9de246e686f02568a2c45e7aa5110166778d
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:36:37 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int z=10; //global variable
+
+int sum(int a, int b) {
+	
+}
+
+
+
+int main () {
+	int i,j;
+	i=10,j=20;
+	int s=sum(i,j);
+	printf("sum=%d\n",s);
+	printf("z=%d\n",z);
+	
+	return 0;
+	
+}
+
+
