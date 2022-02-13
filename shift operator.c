2631256c08b4f13b8ab733f0b4afa14656aee034
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:51:17 +0530

    Add files via upload

---
+++ shift operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+	int	a=60,c=0; //111100
+				  11110000;				  
+	c=a<<2; //left shift operator 
+	 printf("%d\n",c);
+	c=a>>2; //right shift operator  
+					//111100
+		printf("%d\n",c);
+
+}
