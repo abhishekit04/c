1106ea2af9ae7c076ddc051913451c3e7992c5f8
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:47:30 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+
+int main () {
+	char str[] = {'H','E','L','L','O'};
+	printf("\n%c\n%c\n%c\n%c\n%c\n",str[0],str[1],str[2],str[3],str[4]);
+	
+	
+	return 0;
+} 
