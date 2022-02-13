c0abd6251ac7012081d550db0444c18e42b0b58f
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:43:52 +0530

    Add files via upload

---
+++ sum_function.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+
+
+void myfunction(char country ) {
+	printf("%c",country);
+}
+
+int main () {
+	myfunction('S');
+	myfunction('I');
+	myfunction('U');
+	return 0;
+}
---
+++ sum_of_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main()
+{
+    sum_of_numbers();
+    // float value = findSI();
+    // printf("%.2f", value);
+
+    return 0;
+}
