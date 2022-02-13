1fbed26ea7c55791c5343808c8d9f257775b5c81
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:39:41 +0530

    Add files via upload

---
+++ three numbers.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main () {
+	int num1,num2,num3;
+	
+	printf("Enter three numbers\n");
+	scanf("%d%d%d",&num1,&num2,&num3);
+	
+	if(num1<num2) {
+		if(num1<num3) {
+			printf("%d is the smallest number\n",num1);
+		}
+		else {
+			printf("%d is a smallest number\n",num3);
+		}
+	}
+	else if(num2<num1) {
+		if(num2<num3) {
+			printf("%d is a smallest number\n",num2);
+		}
+		else {
+			printf("%d is a smallest number\n",num3);
+		}
+	}
+	if(num1>num2) {
+		if(num1>num3) {
+			printf("%d is the largest number\n",num1);
+		}
+		else {
+			printf("%d is a largest number\n",num3);
+		}
+	}
+	else if(num2>num1) {
+		if(num2>num3) {
+			printf("%d is a largest number\n",num2);
+		}
+		else {
+			printf("%d is a largest number\n",num3);
+		}
+	}
+	return 0;
+}
