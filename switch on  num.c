423d3abe4ecfbe837195d1cb745fde194d2a0735
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:43:15 +0530

    Add files via upload

---
+++ switch on  num.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main () {
+	int num;
+	printf("enter a number");
+	scanf("%d",num);
+	
+	switch(num)
+	{
+		case 1 :
+			printf("case 1 excituted");
+			break;
+			
+		case 2 :
+		    printf("case 2 excituted");
+			break;
+		
+		case 3 :
+		   printf("case 3 excituted");
+		   break;		
+			
+	}
+	
+	return 0;
+}
