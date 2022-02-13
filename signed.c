9e7e28459d2fd29fdbaf0dd9a2a2c3ebf66ae625
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:51:04 +0530

    Add files via upload

---
+++ signed.c
@@ -0,0 +1,15 @@
+// Programing on number divisible by 5 and 3
+
+#include<stdio.h>
+
+int main () {
+	int num;
+	printf("enter a number\n");
+	scanf("%d",&num);
+	
+	printf("%d",num*num);
+	
+
+	
+	return 0 ;
+}
