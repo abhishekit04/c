011ad626f97c78e86131fed1002d714f187d2ed5
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:48:21 +0530

    Add files via upload

---
+++ stack__as_an_array.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+#define MAX 10 
+
+struct stack {
+    int arr[MAX];
+    int top;
+};
+
+void initstack (struct stack *);
+void push (struct stack *, int item);
+int pop(struct stack *);
+
+int main() {
+    struct stack s;
+    int i;
+    system ("cls");
+
+    initstack (&s);
+
+    
+
+    push(&s,11);
+    push(&s,23);
+    push(&s,-8);
+    push(&s,16);
+    push(&s,27);
+    push(&s,1);
+    push(&s,20);
+    push(&s,39);
+    push(&s,2);
+    push(&s,15);
+    push(&s,7);
+    
+    i = pop(&s);
+    if (i !=NULL)
+        printf("item popped: %d\n",i);
+
+    i = pop (&s);
+    if(i != NULL)
+        printf("item popped:%d\n",i);
+
+    i = pop (&s);
+    if (i != NULL)
+        printf("item popped:%d\n",i);
+
+    return 0;
+}
+
+
+void initstack(struct stack *s){
+    s-> top = -1;
+}
+
+
+void push(struct stack *s, int item){
+    if (s -> top == MAX - 1)
+    {
+        printf ("stack is full.\n");
+        return;
+    }
+    s -> top++;
+    s -> arr[s->top] = item;
+}
+
+int pop (struct stack *s)
+{
+    int data;
+    if(s->top == -1){
+        printf("stack is empty.\n");
+        return NULL;
+    }
+    data = s-> arr[s-> top];
+    s->top--;
+    return data;
+}
+
+
