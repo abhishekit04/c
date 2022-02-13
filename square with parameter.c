0be6d8b5079a3a2de71b9c7ea3c931b9bcec1297
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-14 00:49:17 +0530

    Add files via upload

---
+++ square with parameter.c
@@ -0,0 +1,14 @@
+//write a program to find the square of any number using the function with parameter
+
+#include<stdio.h>
+
+int main() {
+	int i=5;
+	printf("square of %d = %d",i,square(i));
+
+
+}
+
+int square(int a) {
+	return a*a;
+}
