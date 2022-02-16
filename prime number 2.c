cb8bbe78da0f14e421a038ea3943a67bda503eac
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-16 23:59:27 +0530

    Add files via upload

---
+++ prime number 2.c
@@ -0,0 +1,21 @@
+// program on prime number
+
+#include<stdio.h>
+
+int main () {
+	int i,j;
+	
+	printf("write a number\n");
+	scanf("%d",&i);
+	
+	for(i=2;i<=j/2;i++) {
+		if(i == 1) {
+			printf("%d is not a prime number");
+		}
+		else {
+			printf("%d is a prime number");
+		}
+	}
+	
+	return 0;
+}
