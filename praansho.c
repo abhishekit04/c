ebaa66503acd7b8337f3302686222b4eedafabf4
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-17 23:50:10 +0530

    Add files via upload

--- praansho.c
+++ praansho.c
@@ -1,41 +1,21 @@
-#include<graphics.h>
-#include<conio.h>
+//syntax
+//
+//for(initialization; condition; iteration) {
+//	statments;
+//}
+//initialization portion of the loop set a loop control variable to an initial value .
+//
+//condition : is a boolean expression that tests the loop control varibale 
+//if the outcome of that test is true , the for loop continue to iterate . if it is false the loop terminates
+//
+//the iteration expression determines how the loop control variable is changed each time the loop iterates.
+
 #include<stdio.h>
 
-int main() {
-		int i;
-		int gm = DETECT,gd;
-
-
-		initgraph(&gm,&gd,"C:\\TC\\BGI");
-		
-		line(0,25,0,75);
-		rectangle(0,25,25,50);
-		line(50,25,50,75);
-		rectangle(50,25,75,50);
-		line(50,50,75,75);
-		line(125,25,100,75);
-		line(125,25,150,75);
-		line(112,50,137,50);
-		line(187,50,212,50);
-		line(200,25,175,75);
-		line(200,25,225,75);
-		line(250,25,250,75);
-		line(250,25,275,75);
-		line(275,25,275,75);
-		line(300,25,325,25);
-		line(300,50,325,50);
-		line(300,75,325,75);
-		line(300,25,300,50);
-		line(325,50,325,75);
-		line(350,25,350,75);
-		line(375,25,375,75);
-		line(350,50,375,50);
-		circle(425,50,25);
-
-
-		getch();
-		closegraph();
-		return 0;
-
+int main () {
+	int i;
+	for(i=1;i<=10;i++) {
+		printf("%d\n",i);
+	}
+	return 0;
 }
