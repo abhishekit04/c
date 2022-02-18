1944fcdf95633e1b4f95615d70522afe38924eaf
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-18 09:43:54 +0530

    Add files via upload

---
+++ output find.c
@@ -0,0 +1,28 @@
+////1 c
+////
+////4
+//
+#include<stdio.h>
+//
+//int main() {
+//	int a;
+//	for(a = 1; a < 5; a++) {
+//		if(a == 2) {
+//			continue;
+//		}
+//		printf("*");
+//	}
+//	
+//	return 0;
+//} 
+
+void number () {
+	int num = 72;
+}
+
+int main () {
+	number() ;
+	printf("num = %d", num);
+	return 0;
+}
+
