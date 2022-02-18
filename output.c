bbb8e1071906940d1e0a0bf25c27215aa33d67a2
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-18 09:43:23 +0530

    Add files via upload

---
+++ output.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main () {
+	int a=5,b=3;
+	//printf("%d",++(a*b+1));
+	printf("%d\n",++ a);
+	printf("%d\n",++b);
+//	printf("%d",++1);
+
+
+
+	return 0;
+	
+}
+/* 
+		
+for increment or decrement operator . operand can be only a variable , the operand cannot be an expression
+or a constant value 
+
+
+ */
