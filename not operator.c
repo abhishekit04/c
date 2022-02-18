7986ce4417012abf05c95e75c5ee323412532787
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-18 09:45:08 +0530

    Add files via upload

---
+++ not operator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	int y = 9;
+	if(!(y=9)) {
+		printf("y is not equal to  9");
+	}
+	else {
+		printf("y is  equal to  9");
+	}
+	
+	getch();
+	return 0;
+}
