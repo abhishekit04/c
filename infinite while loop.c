f689a363ca2489fc7996fdcebba0b4d117b856e5
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-24 09:59:40 +0530

    Add files via upload

---
+++ infinite while loop.c
@@ -0,0 +1,12 @@
+//while loop as an infinite loop
+// if we do not write the increment or decrement statments in a while loop,it becames an infinite loop
+#include<stdio.h>
+
+int main() {
+	int i = 0;
+	while(i<=50){
+		printf("%d\n",i);
+		
+	}
+	return 0;
+}
