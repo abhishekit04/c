9a9f7271647dcaa43774093036ec617a758a2466
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-24 09:59:53 +0530

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,23 @@
+// increment
+
+#include<stdio.h>
+#include<conio.h>
+
+int main () {
+	int num = 5;
+	printf("%d\n", num);
+//	num = num+2;
+//	num += 2;
+	// post increment
+//	num++;
+	// pre increment  
+	
+//	printf("%d\n", ++num);
+	
+	printf("%d\n", num++);
+	printf("%d\n", num);
+	
+	
+	getch ();
+	return 0;
+}
