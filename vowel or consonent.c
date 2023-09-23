43bb582784d635c9c46ccfe83ba7ec8e8be52563
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2023-09-23 18:12:11 +0530

    Add files via upload

---
+++ vowel or consonent.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+int main() {
+	char alphabet;
+	printf("Enter alphabet\n");
+	scanf("%c",&alphabet);
+	
+	switch(alphabet) {
+		case 'A':
+		case 'a':
+		case 'E':
+		case 'e':
+		case 'I':
+		case 'i':
+		case 'O':
+		case 'o':
+		case 'U':
+		case 'u':
+			printf("Vowel");
+			break;
+		case 'B':
+		case 'b':
+		case 'C':
+		case 'c':
+		case 'D':
+		case 'd':
+		case 'F':
+		case 'f':
+		case 'G':
+		case 'g':
+		case 'H':
+		case 'h':
+		case 'J':
+		case 'j':
+		case 'K':
+		case 'k':
+		case 'L':
+		case 'l':
+		case 'M':
+		case 'm':
+		case 'N':
+		case 'n':
+		case 'P':
+		case 'p':
+		case 'Q':
+		case 'q':
+		case 'R':
+		case 'r':
+		case 'S':
+		case 's':
+		case 'T':
+		case 't':
+		case 'V':
+		case 'v':
+		case 'W':
+		case 'w':
+		case 'X':
+		case 'x':
+		case 'Y':
+		case 'y':	
+		case 'Z':
+		case 'z':
+			printf("Consonent\n");
+		default:
+			printf("not a alphabet");
+			break;
+	}
+	
+	return 0;
+}
